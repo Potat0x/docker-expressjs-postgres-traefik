d4557e4ba09efe13ae134f913a1aea6f4a1bf4c4
Potat0x <Potat0x@users.noreply.github.com>
2019-05-25 21:58:07 +0200

    Working separately

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+const pgp = require('pg-promise')(/* options */)
+//const db = pgp('postgres://username:password@host:port/database')
+const db = pgp('postgres://postgres:docker@localhost:5432/postgres');
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/pgtest', (req, res) => {
+
+    return db.any('SELECT * FROM table1;')
+        .then(function (data) {
+            console.log('DATA:', data);
+            res.send(data);
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error);
+            res.send(error);
+        });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
---
+++ database_init.sql
@@ -0,0 +1,4 @@
+create table table1(a text, b text);
+insert into table1 values('a1', 'b1');
+insert into table1 values('a2', 'b2');
+select * from table1;
