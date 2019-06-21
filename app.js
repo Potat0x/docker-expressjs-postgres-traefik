501415ea693de47eedc82399c0b34fb81d01403d
Potat0x <Potat0x@users.noreply.github.com>
2019-06-22 00:25:52 +0200

    Multiple containers, postgres, traefik

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const os = require('os');
 const app = express()
 const port = 8080
 
 const pgp = require('pg-promise')(/* options */)
 //const db = pgp('postgres://username:password@host:port/database')
-const db = pgp('postgres://postgres:docker@pg-docker:5432/postgres');
+// const databaseHost = "127.0.0.1";
+const databaseHost = process.env.POSTGRES_HOST;
+const databasePassword = process.env.POSTGRES_PASSWORD;
+const db = pgp(`postgres://postgres:${databasePassword}@${databaseHost}:5432/postgres`);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`Hello World!<br>${os.hostname()}<br>${os.uptime()}`))
 app.get('/pgtest', (req, res) => {
 
     return db.any('SELECT * FROM table1;')
