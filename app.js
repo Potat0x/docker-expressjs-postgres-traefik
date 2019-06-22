8a001069f8a8feea436a769ac1a816eeb00736a3
Potat0x <Potat0x@users.noreply.github.com>
2019-06-22 17:33:26 +0200

    Cleanup

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-const express = require('express')
+const express = require('express');
 const os = require('os');
-const app = express()
-const port = 8080
+const pgp = require('pg-promise')();
 
-const pgp = require('pg-promise')(/* options */)
-//const db = pgp('postgres://username:password@host:port/database')
-// const databaseHost = "127.0.0.1";
+const app = express();
+const port = 8080;
+
 const databaseHost = process.env.POSTGRES_HOST;
 const databasePassword = process.env.POSTGRES_PASSWORD;
+
+// postgres://username:password@host:port/database
 const db = pgp(`postgres://postgres:${databasePassword}@${databaseHost}:5432/postgres`);
 
-app.get('/', (req, res) => res.send(`Hello World!<br>${os.hostname()}<br>${os.uptime()}`))
+app.get('/', (req, res) => res.send(`Hello World!<br>${os.hostname()}<br>${os.uptime()}`));
 app.get('/pgtest', (req, res) => {
-
-    return db.any('SELECT * FROM table1;')
-        .then(function (data) {
+    return db.any('SELECT * FROM hello_world;')
+        .then(data => {
             console.log('DATA:', data);
             res.send(data);
         })
-        .catch(function (error) {
+        .catch(error => {
             console.log('ERROR:', error);
             res.send(error);
         });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- database_init.sql
+++
@@ -1,4 +0,0 @@
-create table table1(a text, b text);
-insert into table1 values('a1', 'b1');
-insert into table1 values('a2', 'b2');
-select * from table1;
